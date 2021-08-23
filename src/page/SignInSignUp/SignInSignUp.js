48f8ac88e08553d953f2ed312fba40048ee22bf8
Guso Olivares <guso008@gmail.com>
2021-08-23 20:19:05 -0300

    Changes in SignIn-Up

--- src/page/SignInSignUp/SignInSignUp.js
+++ src/page/SignInSignUp/SignInSignUp.js
@@ -1,39 +1,59 @@
-import React from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import React from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSearch,
+  faUsers,
+  faComment,
+} from "@fortawesome/free-solid-svg-icons";
 import LogoTwittor from "../../assets/png/logo.png";
 import LogoWhiteTwitter from "../../assets/png/logo-white.png";
-import "./SignInSignUp.scss"
 
+import "./SignInSignUp.scss";
 
 export default function SignInSignUp() {
-    return (
-        <Container className="signin-signup" fluid>
-            <Row>
-                <LeftComponent />
-                <RightComponent />
-            </Row>
-        </Container>
-    )
+  return (
+    <Container className="signin-signup" fluid>
+      <Row>
+        <LeftComponent />
+        <RightComponent />
+      </Row>
+    </Container>
+  );
 }
 
 function LeftComponent() {
-    return (
-        <Col className="signin-signup__left" xs={6}>
-            <img src={LogoTwittor} alt="Twittor" />
-            <div>
-                <h2>- Sigue lo que te interesa</h2>
-                <h2>- Enterate lo que está pasando</h2>
-                <h2>- Unete a la conversación</h2>
-            </div>
-        </Col>
-    );
+  return (
+    <Col className="signin-signup__left" xs={6}>
+      <img src={LogoTwittor} alt="Twittor" />
+      <div>
+        <h2>
+          <FontAwesomeIcon icon={faSearch} />
+          Sigue lo que te interesa.
+        </h2>
+        <h2>
+          <FontAwesomeIcon icon={faUsers} />
+          Entérate de qué está hablando la gente.
+        </h2>
+        <h2>
+          <FontAwesomeIcon icon={faComment} />
+          Únete a la conversación.
+        </h2>
+      </div>
+    </Col>
+  );
 }
 
-
 function RightComponent() {
-    return (
-        <Col className="signin-signup__right">
-            <h2>RightComponent...</h2>
-        </Col>
-    );
+  return (
+    <Col className="signin-signup__right" xs={6}>
+      <div>
+        <img src={LogoWhiteTwitter} alt="Twittor" />
+        <h2>Mira lo que está pasando en el mundo en este momento</h2>
+        <h3>Unete a Twittor hoy mismo</h3>
+        <Button variant="primary">Regístrate</Button>
+        <Button variant="outline-primary">Iniciar Sessión</Button>
+      </div>
+    </Col>
+  );
 }
