c68164cba70f3120ad99c755b8f6efc02f7461cf
Guso Olivares <guso008@gmail.com>
2021-08-23 00:47:10 -0300

    Left SignIn-Up

--- src/page/SignInSignUp/SignInSignUp.js
+++ src/page/SignInSignUp/SignInSignUp.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
+import LogoTwittor from "../../assets/png/logo.png";
+import LogoWhiteTwitter from "../../assets/png/logo-white.png";
+import "./SignInSignUp.scss"
 
 
 export default function SignInSignUp() {
@@ -15,8 +18,13 @@ export default function SignInSignUp() {
 
 function LeftComponent() {
     return (
-        <Col className="signin-signup__left">
-            <h2>LeftComponent...</h2>
+        <Col className="signin-signup__left" xs={6}>
+            <img src={LogoTwittor} alt="Twittor" />
+            <div>
+                <h2>- Sigue lo que te interesa</h2>
+                <h2>- Enterate lo que está pasando</h2>
+                <h2>- Unete a la conversación</h2>
+            </div>
         </Col>
     );
 }
