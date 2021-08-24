abc7c28aaab417f59310191b549e21839e0de401
Guso Olivares <guso008@gmail.com>
2021-08-24 00:23:37 -0300

    Implements OnClick Modals

--- src/page/SignInSignUp/SignInSignUp.js
+++ src/page/SignInSignUp/SignInSignUp.js
@@ -13,20 +13,24 @@ import LogoWhiteTwitter from "../../assets/png/logo-white.png";
 import "./SignInSignUp.scss";
 
 export default function SignInSignUp() {
-  const [showModal, setShowModal] = useState(true);
+  const [showModal, setShowModal] = useState(false);
   const [contentModal, setContentModal] = useState(null);
+
+  const openModal = (content) => {
+    setShowModal(true);
+    setContentModal(content);
+  };
+
   return (
     <>
       <Container className="signin-signup" fluid>
         <Row>
           <LeftComponent />
-          <RightComponent />
+          <RightComponent openModal={openModal} setShowModal={setShowModal} />
         </Row>
       </Container>
       <BasicModal show={showModal} setShow={setShowModal}>
-        <div>
-          <h2>Modal Content</h2>
-        </div>
+        {contentModal}
       </BasicModal>
     </>
   );
@@ -54,15 +58,26 @@ function LeftComponent() {
   );
 }
 
-function RightComponent() {
+function RightComponent(props) {
+  const { openModal, setShowModal } = props;
   return (
     <Col className="signin-signup__right" xs={6}>
       <div>
         <img src={LogoWhiteTwitter} alt="Twittor" />
         <h2>Mira lo que está pasando en el mundo en este momento</h2>
         <h3>Unete a Twittor hoy mismo</h3>
-        <Button variant="primary">Regístrate</Button>
-        <Button variant="outline-primary">Iniciar Sessión</Button>
+        <Button
+          variant="primary"
+          onClick={() => openModal(<h2>Formulario de Registro</h2>)}
+        >
+          Regístrate
+        </Button>
+        <Button
+          variant="outline-primary"
+          onClick={() => openModal(<h2>Formulario de Login</h2>)}
+        >
+          Iniciar Sessión
+        </Button>
       </div>
     </Col>
   );
