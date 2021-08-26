a70980d95214e58ad8853022c5ec9649292b024f
Guso Olivares <guso008@gmail.com>
2021-08-26 00:22:54 -0300

    Add logical save formData

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -1,41 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import { Row, Col, Form, Button, Spinner } from "react-bootstrap";
 import "./SignUpForm.scss";
 
 export default function SignUpForm(props) {
   const { setShowModal } = props;
+  const [formData, setFormData] = useState(initialFormValue());
 
   const onSubmit = (e) => {
     e.preventDefault();
     setShowModal(false);
+
+    console.log(formData);
+  };
+
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   };
   return (
     <div className="sign-up-form">
       <h2>Crea tu cuenta</h2>
-      <Form onSubmit={onSubmit}>
+      <Form onSubmit={onSubmit} onChange={onChange}>
         <Form.Group>
           <Row>
             <Col>
-              <Form.Control type="text" placeholder="Nombre" />
+              <Form.Control
+                type="text"
+                placeholder="Nombre"
+                name="nombre"
+                defaultValue={formData.nombre}
+              />
             </Col>
             <Col>
-              <Form.Control type="text" placeholder="Apellidos" />
+              <Form.Control
+                type="text"
+                placeholder="Apellidos"
+                name="apellidos"
+                defaultValue={formData.apellido}
+              />
             </Col>
           </Row>
         </Form.Group>
         <Form.Group>
           <Form.Label></Form.Label>
-          <Form.Control type="email" placeholder="Correo electronico" />
+          <Form.Control
+            type="email"
+            placeholder="Correo electronico"
+            name="email"
+            defaultValue={formData.email}
+          />
         </Form.Group>
         <Form.Group>
           <Row>
             <Col>
               <Form.Label></Form.Label>
-              <Form.Control type="password" placeholder="Contraseña" />
+              <Form.Control
+                type="password"
+                placeholder="Contraseña"
+                name="password"
+                defaultValue={formData.password}
+              />
             </Col>
             <Col>
               <Form.Label></Form.Label>
-              <Form.Control type="password" placeholder="Repetir contraseña" />
+              <Form.Control
+                type="password"
+                placeholder="Repetir contraseña"
+                name="repeatPassword"
+                defaultValue={formData.repeatPassword}
+              />
             </Col>
           </Row>
         </Form.Group>
@@ -45,4 +77,14 @@ export default function SignUpForm(props) {
       </Form>
     </div>
   );
+}
+
+function initialFormValue() {
+  return {
+    nombre: "",
+    apellidos: "",
+    email: "",
+    password: "",
+    repeatPassword: "",
+  };
 }
