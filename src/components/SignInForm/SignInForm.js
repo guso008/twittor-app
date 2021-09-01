417401b2b5d9368b83aa6faeac93e620612ce699
Guso Olivares <guso008@gmail.com>
2021-09-01 20:45:13 -0300

    Add formData submit

--- src/components/SignInForm/SignInForm.js
+++ src/components/SignInForm/SignInForm.js
@@ -1,29 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button, Spinner } from "react-bootstrap";
 
 import "./SignInForm.scss";
 
 export default function SignInForm() {
+  const [formData, setFormData] = useState(initialFormValue());
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log("Login...");
+    console.log(formData);
+  };
+
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
   return (
     <div className="sign-in-form">
       <h2>Entrar</h2>
-      <Form onSubmit={onSubmit}>
+      <Form onSubmit={onSubmit} onChange={onChange}>
         <Form.Group>
-          <Form.Control type="email" placeholder="Correo electronico" />
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Correo electronico"
+            defaultValue={formData.email}
+          />
         </Form.Group>
         <Form.Group>
           <Form.Label></Form.Label>
-          <Form.Control type="password" placeholder="Contraseña" />
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Contraseña"
+            defaultValue={formData.password}
+          />
         </Form.Group>
+        <Button variant="primary" type="submit">
+          Iniciar Session
+        </Button>
       </Form>
-      <Button variant="primary" type="submit">
-        Iniciar Session
-      </Button>
     </div>
   );
+}
+
+function initialFormValue() {
+  return {
+    email: "",
+    password: "",
+  };
 }
