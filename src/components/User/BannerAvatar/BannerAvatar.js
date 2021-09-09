0b7c170e11800882c42d6ba979ef3544d0e26641
Guso Olivares <guso008@gmail.com>
2021-09-09 19:56:42 -0300

    Add ConfigModal

--- src/components/User/BannerAvatar/BannerAvatar.js
+++ src/components/User/BannerAvatar/BannerAvatar.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "react-bootstrap";
+import ConfigModal from "../../Modal/ConfigModal/ConfigModal";
 import AvatarNotFound from "../../../assets/png/avatar-no-found.png";
 import { API_HOST } from "../../../utils/constant";
 import "./BannerAvatar.scss";
 
 export default function BannerAvatar(props) {
   const { user, loggedUser } = props;
+  const [showModal, setShowModal] = useState(false);
   const bannerUrl = user?.banner
     ? `${API_HOST}/obtenerBanner?id=${user.id}`
     : null;
@@ -27,10 +29,19 @@ export default function BannerAvatar(props) {
       />
       {user && (
         <div className="options">
-          {loggedUser._id === user.id && <Button>Editar perfil</Button>}
+          {loggedUser._id === user.id && (
+            <Button onClick={() => setShowModal(true)}>Editar perfil</Button>
+          )}
           {loggedUser._id !== user.id && <Button>Seguir</Button>}
         </div>
       )}
+      <ConfigModal
+        show={showModal}
+        setShow={setShowModal}
+        title="Editar Perfil"
+      >
+        Formulario de Edición...
+      </ConfigModal>
     </div>
   );
 }
