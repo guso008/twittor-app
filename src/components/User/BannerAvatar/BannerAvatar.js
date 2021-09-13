4f58a60b92ea5c74cf78e55e0fe6dc75c8ea073f
Guso Olivares <guso008@gmail.com>
2021-09-13 19:34:32 -0300

    Add Follow, Unfollow

--- src/components/User/BannerAvatar/BannerAvatar.js
+++ src/components/User/BannerAvatar/BannerAvatar.js
@@ -1,14 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button } from "react-bootstrap";
 import ConfigModal from "../../Modal/ConfigModal/ConfigModal";
 import EditForm from "../EditUserForm/EditForm";
 import AvatarNotFound from "../../../assets/png/avatar-no-found.png";
 import { API_HOST } from "../../../utils/constant";
+import {
+  checkFollowApi,
+  followUserApi,
+  unfollowUserApi,
+} from "../../../api/follow";
+
 import "./BannerAvatar.scss";
 
 export default function BannerAvatar(props) {
   const { user, loggedUser } = props;
   const [showModal, setShowModal] = useState(false);
+  const [following, setFollowing] = useState(null);
+  const [reloadFollow, setReloadFollow] = useState(false);
+
   const bannerUrl = user?.banner
     ? `${API_HOST}/obtenerBanner?id=${user.id}`
     : null;
@@ -17,8 +26,32 @@ export default function BannerAvatar(props) {
     ? `${API_HOST}/obtenerAvatar?id=${user.id}`
     : AvatarNotFound;
 
-  console.log(user);
-  console.log(loggedUser);
+  useEffect(() => {
+    if (user) {
+      checkFollowApi(user?.id).then((response) => {
+        console.log(response);
+        if (response?.status) {
+          setFollowing(true);
+        } else {
+          setFollowing(false);
+        }
+      });
+    }
+    setReloadFollow(false);
+  }, [user, reloadFollow]);
+
+  const onFollow = () => {
+    followUserApi(user.id).then(() => {
+      setReloadFollow(true);
+    });
+  };
+
+  const onUnfollow = () => {
+    unfollowUserApi(user.id).then(() => {
+      setReloadFollow(true);
+    });
+  };
+
   return (
     <div
       className="banner-avatar"
@@ -33,7 +66,15 @@ export default function BannerAvatar(props) {
           {loggedUser._id === user.id && (
             <Button onClick={() => setShowModal(true)}>Editar perfil</Button>
           )}
-          {loggedUser._id !== user.id && <Button>Seguir</Button>}
+          {loggedUser._id !== user.id &&
+            following !== null &&
+            (following ? (
+              <Button onClick={onUnfollow} className="unfollow">
+                <span>Siguiendo</span>
+              </Button>
+            ) : (
+              <Button onClick={onFollow}>Seguir</Button>
+            ))}
         </div>
       )}
       <ConfigModal
